ab18ca05c626578ad8c2d4f2de68a2c5cfb1c768
Betty Moreschini <betty.moreschini@gmail.com>
2023-03-13 15:29:27 +0100

    add models (#1)

---
+++ src/main/java/org/mathem/deliverydates/models/Delivery.java
@@ -0,0 +1,15 @@
+package org.mathem.deliverydates.models;
+
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Delivery {
+  private int postalCode;
+  private LocalDate deliveryDate;
+  private boolean isGreenDelivery;
+}
